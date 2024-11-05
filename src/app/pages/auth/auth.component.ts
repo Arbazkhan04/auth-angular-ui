a38673016766bccca0cf6520a33f2d435d9fd593
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-05 15:34:00 +0500

    add

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -17,19 +17,21 @@ export class AuthComponent implements AfterViewInit {
   toggleForm() {
     this.animateToggle();
   }
-
   animateToggle() {
     const leftContainer = document.querySelector('.left-container') as HTMLElement;
     const rightContainer = document.querySelector('.right-container') as HTMLElement;
     const leftContainerContent = leftContainer.querySelector('.left-container-content') as HTMLElement;
     const rightContainerContent = rightContainer.querySelector('.right-container-content') as HTMLElement;
-
+  
     const tl = gsap.timeline();
-
+  
+    // Set the initial position to ensure no residual movement
+    gsap.set(leftContainerContent, { x: 0, clearProps: "transform" });
+  
     // Define the target width for both containers
     const currentWidth = leftContainer.offsetWidth;
     const targetWidth = currentWidth * 2;
-
+  
     if (!this.isSignUpVisible) {
       // Animate to Sign Up form
       tl.to(leftContainer, {
@@ -39,26 +41,26 @@ export class AuthComponent implements AfterViewInit {
       })
         .to(leftContainer, {
           x: 150,
-          duration: 1,
           scaleX: 1.5,
+          duration: 1,
           ease: 'power2.out',
-          delay: -1.5,
+          delay: -0.8,
         })
         .to(rightContainer, {
           x: 0,
-          duration: 1.5,
+          duration: 1,
           ease: 'power2.out',
-          delay: -1.5,
+          delay: -1,
         })
         .to(leftContainerContent, {
-          x: -800,
-          duration: 0.5,
+          x: -700, // Moves only left
+          duration: 1,
           ease: 'power2.out',
-          delay: -1.5,
+          delay: -1,
         })
         .set(rightContainer, { display: 'none' })
         .set(leftContainer, { xPercent: 0 });
-
+  
       tl.eventCallback("onComplete", () => {
         this.isSignUpVisible = true;
       });
@@ -66,18 +68,19 @@ export class AuthComponent implements AfterViewInit {
       // Animate to Sign In form
       gsap.set(rightContainer, { display: 'flex' });
       const tl = gsap.timeline();
-
+  
       tl.to(rightContainer, {
         width: targetWidth + 'px',
-        duration: 1,
+        duration: 1.5,
         ease: 'power2.out',
-      }).to(rightContainer, {
-        x: -200,
-        scaleX: 2,
-        duration: 1,
-        ease: 'power2.out',
-        delay: -1.5,
       })
+        .to(rightContainer, {
+          x: -200,
+          scaleX: 2,
+          duration: 1.5,
+          ease: 'power2.out',
+          delay: -1.5,
+        })
         .to(leftContainer, {
           x: 0,
           duration: 1.5,
@@ -85,18 +88,20 @@ export class AuthComponent implements AfterViewInit {
           delay: -1.5,
         })
         .to(rightContainerContent, {
-          x: 800,
-          duration: 0.5,
+          x: 600,
+          duration: 1.5,
           ease: 'power2.out',
           delay: -1.5,
         })
         .set(leftContainer, { display: 'flex' });
-
+  
       tl.eventCallback("onComplete", () => {
         this.isSignUpVisible = false;
       });
     }
   }
+  
+
 
 
   ngAfterViewInit() {
